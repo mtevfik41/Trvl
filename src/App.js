91a2dc401dca8d484ccd0fa2d9e4319868bf41a1
Tevfik Kadan <mtevfik41@gmail.com>
2021-08-19 15:46:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Fragment>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path='/Trvl' exact component={Home} />
+            <Route path='/Trvl/services' component={Services} />
+            <Route path='/Trvl/products' component={Products} />
+            <Route path='/Trvl/sign-up' component={SignUp} />
+          </Switch>
+        </Router>
+      </Fragment>
   );
 }
 
